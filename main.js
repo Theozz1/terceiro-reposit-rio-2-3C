a6389ee3901428baedd845af17ca66d7740d8303
Theozz1 <theo.perez@escola.pr.gov.br>
2025-07-07 08:21:27 -0300

    07/07 - 2

--- main.js
+++ main.js
@@ -58,10 +58,10 @@ function geraSenha() {
 }
 
 function classificaSenha() {
-    forcaSenha classList.remove('fraca', 'media', 'forte');
+    forcaSenha = classList.remove('fraca', 'media', 'forte');
     if (tamanhoSenha>11){
         forcaSenha.classList.add('forte');
-    } else if(tamanhoSenha>5 %% tamanhoSenha<12) {
+    } else if(tamanhoSenha>5 && tamanhoSenha<12) {
         forcaSenha.classList.add('media');
     } else if(tamanhoSenha<=5){
         forcaSenha.classList.add('fraca');
